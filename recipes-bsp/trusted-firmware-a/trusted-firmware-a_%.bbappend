dh-electronics/meta-dhsom-stm32-bsp

commit a6101ce5ca4c97034a2d005c7858517bbf2c1f1f

tfa: Replace do_compile[depends] with DEPENDS:append
Use DEPENDS:append:dh-stm32mp13-dhcor-dhsbc instead of do_compile[depends]
as the DEPENDS:append:dh-stm32mp13-dhcor-dhsbc can be limited to specific
MACHINE, in this case dh-stm32mp13-dhcor-dhsbc. This removes a side-effect
this layer might have caused by the do_compile[depends] that is applied to
all MACHINEs.

Signed-off-by: Marek Vasut <marex@denx.de>

recipes-bsp/trusted-firmware-a/trusted-firmware-a_%.bbappend
@@ -4,7 +4,7 @@ TFA_PLATFORM:dh-stm32mp13-dhcor-dhsbc = "stm32mp1"
 TFA_BUILD_TARGET:dh-stm32mp13-dhcor-dhsbc = "all fip"
 TFA_INSTALL_TARGET:dh-stm32mp13-dhcor-dhsbc = "tf-a-stm32mp135f-dhcor-dhsbc.stm32 fip.bin"
 
-do_compile[depends] += "optee-os:do_deploy u-boot-mainline:do_deploy"
+DEPENDS:append:dh-stm32mp13-dhcor-dhsbc = "optee-os:do_deploy u-boot-mainline:do_deploy"
 
 EXTRA_OEMAKE:append:dh-stm32mp13-dhcor-dhsbc = " \
 	AARCH32_SP=optee \

commit 4695be948c38d2906b6375b8541a21dcf67ef522

tfa: Restore do_compile[depends]
Use do_compile[depends] as we cannot DEPENDS:append on a specific
recipe task, but limit the do_compile[depends] to specific MACHINE,
in this case the dh-stm32mp13-dhcor-dhsbc .

Fixes: a6101ce5ca4c ("tfa: Replace do_compile[depends] with DEPENDS:append")
Signed-off-by: Marek Vasut <marex@denx.de>

recipes-bsp/trusted-firmware-a/trusted-firmware-a_%.bbappend
@@ -4,7 +4,7 @@ TFA_PLATFORM:dh-stm32mp13-dhcor-dhsbc = "stm32mp1"
 TFA_BUILD_TARGET:dh-stm32mp13-dhcor-dhsbc = "all fip"
 TFA_INSTALL_TARGET:dh-stm32mp13-dhcor-dhsbc = "tf-a-stm32mp135f-dhcor-dhsbc.stm32 fip.bin"
 
-DEPENDS:append:dh-stm32mp13-dhcor-dhsbc = " optee-os:do_deploy u-boot-mainline:do_deploy"
+do_compile[depends] += "${@'optee-os:do_deploy u-boot-mainline:do_deploy' if 'dh-stm32mp13-dhcor-dhsbc' in d.getVar('MACHINEOVERRIDES', True).split(':') else ' '}"
 
 EXTRA_OEMAKE:append:dh-stm32mp13-dhcor-dhsbc = " \
 	AARCH32_SP=optee \