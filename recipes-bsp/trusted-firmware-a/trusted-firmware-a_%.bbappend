dh-electronics/meta-dhsom-stm32-bsp

commit a6101ce5ca4c97034a2d005c7858517bbf2c1f1f

tfa: Replace do_compile[depends] with DEPENDS:append
Use DEPENDS:append:dh-stm32mp13-dhcor-dhsbc instead of do_compile[depends]
as the DEPENDS:append:dh-stm32mp13-dhcor-dhsbc can be limited to specific
MACHINE, in this case dh-stm32mp13-dhcor-dhsbc. This removes a side-effect
this layer might have caused by the do_compile[depends] that is applied to
all MACHINEs.

Signed-off-by: Marek Vasut <marex@denx.de>

recipes-bsp/trusted-firmware-a/trusted-firmware-a_%.bbappend
@@ -4,7 +4,7 @@ TFA_PLATFORM:dh-stm32mp13-dhcor-dhsbc = "stm32mp1"
 TFA_BUILD_TARGET:dh-stm32mp13-dhcor-dhsbc = "all fip"
 TFA_INSTALL_TARGET:dh-stm32mp13-dhcor-dhsbc = "tf-a-stm32mp135f-dhcor-dhsbc.stm32 fip.bin"
 
-do_compile[depends] += "optee-os:do_deploy u-boot-mainline:do_deploy"
+DEPENDS:append:dh-stm32mp13-dhcor-dhsbc = "optee-os:do_deploy u-boot-mainline:do_deploy"
 
 EXTRA_OEMAKE:append:dh-stm32mp13-dhcor-dhsbc = " \
 	AARCH32_SP=optee \

commit 6bf3055d7b1e9d2b629b680699a4949031d16964

tfa: Add missing space to DEPENDS:append
It is mandatory to control spacing when using override syntax,
add the missing space.

Fixes: a6101ce5ca4c ("tfa: Replace do_compile[depends] with DEPENDS:append")
Signed-off-by: Marek Vasut <marex@denx.de>

recipes-bsp/trusted-firmware-a/trusted-firmware-a_%.bbappend
@@ -4,7 +4,7 @@ TFA_PLATFORM:dh-stm32mp13-dhcor-dhsbc = "stm32mp1"
 TFA_BUILD_TARGET:dh-stm32mp13-dhcor-dhsbc = "all fip"
 TFA_INSTALL_TARGET:dh-stm32mp13-dhcor-dhsbc = "tf-a-stm32mp135f-dhcor-dhsbc.stm32 fip.bin"
 
-DEPENDS:append:dh-stm32mp13-dhcor-dhsbc = "optee-os:do_deploy u-boot-mainline:do_deploy"
+DEPENDS:append:dh-stm32mp13-dhcor-dhsbc = " optee-os:do_deploy u-boot-mainline:do_deploy"
 
 EXTRA_OEMAKE:append:dh-stm32mp13-dhcor-dhsbc = " \
 	AARCH32_SP=optee \