cd5bbfb784f1434de1b7b90f9f2b545657b8345a
Marek Vasut <marex@nabladev.com>
2026-09-08 23:03:23 +0200

    u-boot: Use bootm start subcommand for DTO application
    The 'bootm start' command is capable of applying DTOs specified
    in the multi component uImage format (FIT) address parameter in
    the same way the current ad-hoc implementation does it, better
    yet, it sets ${fdtaddr} variable to point to the final DT with
    DTOs applied on top, placed in suitable area in memory managed
    by U-Boot.
    
    This eliminates the need for build-time UBOOT_DTB_LOADADDRESS
    and UBOOT_DTBO_LOADADDRESS variables which transcribe into the
    $loadaddrdtb and $loadaddrdtbo environment variables and makes
    the DT and DTO memory management at application time simpler.
    
    Fix up the code comment preceding loaddtoscustom to match the
    actual behavior of the prior script.
    
    Signed-off-by: Marek Vasut <marex@nabladev.com>

--- recipes-bsp/u-boot/files/dh-stm32mp1-dhsom/boot.cmd
+++ recipes-bsp/u-boot/files/dh-stm32mp1-dhsom/boot.cmd
@@ -76,53 +76,14 @@ fi
 
 # A custom script exists to load DTOs
 if test -n "${loaddtoscustom}" ; then
-  if test -z "${loaddtos}" ; then
-    echo "Using base DT from fitImage default configuration as fallback."
-    fdt addr ${loadaddr}
-    fdt get value loaddtos /configurations default
-    setenv loaddtos "#${loaddtos}"
-    echo "To select different base DT to be adjusted using 'loaddtoscustom'"
-    echo "script and passed to Linux kernel, set 'loaddtos' variable:"
-    echo "  => env set loaddtos \#conf@...dtb"
-  fi
-
-  # Matches UBOOT_DTB_LOADADDRESS in OE layer machine config
-  setenv loadaddrdtb %UBOOT_DTB_LOADADDRESS%
-  # Matches UBOOT_DTBO_LOADADDRESS in OE layer machine config
-  setenv loadaddrdtbo %UBOOT_DTBO_LOADADDRESS%
-
-  setexpr loaddtossep gsub '#conf' ' fdt' "${loaddtos}"
-  setexpr loaddtb 1
-
-  for i in ${loaddtossep} ; do
-    if test ${loaddtb} = 1 ; then
-      echo "Using base DT ${i}"
-      imxtract ${loadaddr} ${i} ${loadaddrdtb} ;
-      fdt addr ${loadaddrdtb}
-      fdt resize 0x40000
-      setenv loaddtb 0
-    else
-      echo "Applying DTO ${i}"
-      imxtract ${loadaddr} ${i} ${loadaddrdtbo} ;
-      fdt apply ${loadaddrdtbo}
-    fi
-  done
-
-  setenv loaddtb
-  setenv loaddtossep
-  setenv loadaddrdtbo
-  setenv loadaddrdtb
+  bootm start ${loadaddr}${loaddtos}
 
   # Run the custom DTO loader script
   #
-  # In case 'loaddtos' variable is set, then the 'fdt' command is already
-  # configured to point to a DT, on top of which all the DTOs present in
-  # the fitImage and selected by the 'loaddtos' are applied. Hence, the
-  # user is now free to apply any additional custom DTOs loaded from any
-  # other source.
-  #
-  # In case 'loaddtos' variable is not set, the 'loaddtoscustom' script
-  # must configure the 'fdt' command to point to the custom DT.
+  # The 'fdt' command is already configured to point to a DT, on top
+  # of which all the DTOs present in the fitImage and selected by the
+  # 'loaddtos' are applied. Hence, the user is now free to apply any
+  # additional custom DTOs loaded from any other source.
   run loaddtoscustom
   if test -z "${bootm_args}" ; then
     setenv bootm_args "${loadaddr} - ${fdtaddr}"
